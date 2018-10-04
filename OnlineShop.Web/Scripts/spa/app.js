cbea3db202bb648c1dafd28e1ce750acca7f1bf0
Hồng Ngọc Trương <caulaai1998@gmail.com>
2018-10-04 19:16:40 +0700

    scope,rootscope,scope long nhau (view\admin\index)

--- OnlineShop.Web/Scripts/spa/app.js
+++ OnlineShop.Web/Scripts/spa/app.js
@@ -1,12 +1,42 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 
+//how to use module
+
 var myApp = angular.module('myModule', []);
 
-myApp.controller ("myController", myController);
+//how to use Controller & Scope
 
-myController.$inject = ['$scope'];
+myApp.controller("myController", myController);
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+myApp.controller("houseController", houseController);
+myApp.controller("peopleController", peopleController);
 //declare
+
 function myController($scope) {
     $scope.message = "Math first grade";
 } 
+
+//Scope lồng nhau
+function schoolController($scope) {
+    $scope.message = "Announcement from school";
+} 
+
+function studentController($scope) {
+    $scope.message = "This is message from student";
+} 
+function teacherController($scope) {
+    $scope.message = "This is message from teacher";
+} 
+//rootScope
+function houseController($rootScope,$scope) {
+    $rootScope.message = "From house";
+}
+
+function peopleController($scope) {
+   // $scope.message = "This is message from people";
+} 
+
+
 
